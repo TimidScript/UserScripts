afbe3155a2b9900abfbe2ab998da54d0e164c3b3
TimidScript <xxjack@gmx.com>
2014-08-21 09:05:26 +0100

    Update Youtube Secure.user.js

--- defunct/Youtube Secure.user.js
+++ defunct/Youtube Secure.user.js
@@ -2,14 +2,30 @@
 // @name          [TS] Youtube Secure
 // @namespace     TimidScript
 // @description   **Depreciated** Redirects to Secure (https) Youtube. Depreciated: Use "Linx Amender" instead.
+// @version       1.0.1 DEFUNCT
 // @author        TimidScript
 // @homepageURL   https://openuserjs.org/users/TimidScript
 // @copyright     © 2014 TimidScript, All Rights Reserved.
 // @license       GNU GPLv3 + Please notify me if distributing
-// @version       1.0.1 DEFUNCT
 // @icon          http://i.imgur.com/aqb4avb.png
 // @include       http://*.youtube.*
 // ==/UserScript==
+
+/* Information
+********************************************************************************************
+Copyright © 2014 TimidScript, All Rights Reserved.
+Script's Homepage:              Check homepages below
+
+TimidScript's Homepage:         https://openuserjs.org/users/TimidScript
+                                https://greasyfork.org/users/1455-timidscript
+                                https://monkeyguts.com/author.php?un=timidscript
+                                
+
+                                http://userscripts.org/users/TimidScript
+                                http://userscripts-mirror.org/users/100610/scripts
+
+Initial release date: Jan 10, 2013 14:51
+********************************************************************************************/
 
 var url = document.location.toString();
 document.location = url.replace('http://', 'https://');
