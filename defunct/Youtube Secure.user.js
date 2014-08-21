9b0a0204686eb52e9f58ec6b1dc2033a70c876fd
TimidScript <xxjack@gmx.com>
2014-08-21 09:27:49 +0100

    Update Youtube Secure.user.js

--- defunct/Youtube Secure.user.js
+++ defunct/Youtube Secure.user.js
@@ -3,11 +3,11 @@
 // @namespace     TimidScript
 // @description   **Depreciated** Redirects to Secure (https) Youtube. Depreciated: Use "Linx Amender" instead.
 // @version       1.0.1 DEFUNCT
+// @icon          http://i.imgur.com/aqb4avb.png
 // @author        TimidScript
 // @homepageURL   https://openuserjs.org/users/TimidScript
 // @copyright     © 2014 TimidScript, All Rights Reserved.
 // @license       GNU GPLv3 + Please notify me if distributing
-// @icon          http://i.imgur.com/aqb4avb.png
 // @include       http://*.youtube.*
 // ==/UserScript==
 
