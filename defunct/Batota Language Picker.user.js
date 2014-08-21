6cd4e7adb95b7bd97639d6a4585eda5f1b789867
TimidScript <xxjack@gmx.com>
2014-08-21 09:39:24 +0100

    Update Batota Language Picker.user.js

--- defunct/Batota Language Picker.user.js
+++ defunct/Batota Language Picker.user.js
@@ -3,11 +3,11 @@
 // @namespace           TimidScript
 // @version             1.0.2 DEFUNCT
 // @description         **Depreciated** Only displays one language (Default English). Depreciated: Use "Linx Amender" instead.
+// @icon                http://i.imgur.com/aqb4avb.png
 // @author              TimidScript
 // @homepageURL         https://openuserjs.org/users/TimidScript
 // @copyright           © 2014 TimidScript, All Rights Reserved.
 // @license             GNU GPLv3 + Please notify me if distributing
-// @icon                http://i.imgur.com/aqb4avb.png
 // @include             http://www.batoto.net/comic/*
 // @grant               GM_getValue
 // @grant               GM_setValue
