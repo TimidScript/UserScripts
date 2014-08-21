752546e3d3cef64dfb7a3e2f64dc2b13ecad7bee
TimidScript <xxjack@gmx.com>
2014-08-21 23:25:04 +0100

    Update Pixiv++.user.js

--- scripts/Pixiv++.user.js
+++ scripts/Pixiv++.user.js
@@ -7,7 +7,7 @@
 // @author              TimidScript
 // @homepageURL         https://openuserjs.org/users/TimidScript
 // @copyright           © 2014 TimidScript, All Rights Reserved.
-// @license             GNU GPLv3 + Please notify me if distributing
+// @license             Creative Commons BY-NC-SA + Please notify me if distributing
 // @include             http://www.pixiv.net/*
 // @exclude             http://www.pixiv.net/member_illust.php?mode=manga&illust_id*
 // @exclude             http://www.pixiv.net/member_illust.php?mode=big&illust_id*
