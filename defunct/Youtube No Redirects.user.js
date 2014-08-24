7d9b4a25757cd59fba0b80bec20606f815d72184
TimidScript <xxjack@gmx.com>
2014-08-24 11:40:26 +0100

    Update Youtube No Redirects.user.js

--- defunct/Youtube No Redirects.user.js
+++ defunct/Youtube No Redirects.user.js
@@ -3,7 +3,7 @@
 // @namespace       TimidScript
 // @version         2.0.10 DEFUNCT
 // @description     **Depreciated** Removes Tracker/Redirects from links. Depreciated: Use "Linx Amender" instead.
-// @icon            http://i.imgur.com/aqb4avb.png
+// @icon            https://i.imgur.com/aqb4avb.png
 // @author          TimidScript
 // @homepageURL     https://openuserjs.org/users/TimidScript
 // @copyright       © 2014 TimidScript, All Rights Reserved.
