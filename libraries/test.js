e73e1f83122541a62005f6081694ae94e91dcf8d
TimidScript <xxjack@gmx.com>
2016-02-26 13:15:12 +0000

    Update test.js

--- libraries/test.js
+++ libraries/test.js
@@ -1,1 +1,13 @@
+// ==UserScript==
+// @name                    TSLibrary - Generic
+// @namespace               TimidScript
+// @version                 1.0.16
+// @description             A resource JS library file providing common useful functions to be used by other scripts
+// @author                  TimidScript
+// @homepageURL             https://openuserjs.org/users/TimidScript
+// @copyright               © 2014 TimidScript, All Rights Reserved.
+// @license                 Creative Commons BY-NC-SA + Read the License inside the script
+// @exclude                 *
+// ==/UserScript==
+
 alert("SDDS");
