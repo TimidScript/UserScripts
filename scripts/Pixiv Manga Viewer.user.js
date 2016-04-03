b9860954c4a2d45265fcf59db421ee744b7766d0
TimidScript <xxjack@gmx.com>
2016-04-03 10:00:09 +0100

    2.2.27

--- scripts/Pixiv Manga Viewer.user.js
+++ scripts/Pixiv Manga Viewer.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name            [TS] Pixiv Manga Viewer
 // @namespace       TimidScript
-// @version         2.2.26
+// @version         2.2.27
 // @description     A more Powerful Pixiv Manga Viewer with no lazy loading (loads all images) and better interface. Works with Pixiv++ & Generic Image Viewer ロードし、すべての画像|いいえ遅延読み込み|マンガビューア|ダイレクトリンク|ベターインタフェース
 // @author          TimidScript
 // @homepageURL     https://openuserjs.org/users/TimidScript
 // @copyright       © 2014 TimidScript, All Rights Reserved.
-// @license         Creative Commons BY-NC-SA + Read Copyright inside the script
+// @license         GNU General Public License v3 (GPL-3) + Read the License inside the script
 // @include         http://www.pixiv.net/member_illust.php?mode=manga&illust_id=*
 // @require         https://openuserjs.org/src/libs/TimidScript/TSL_-_Generic.js
 // @require         https://openuserjs.org/src/libs/TimidScript/TSL_-_GM_Update.js
@@ -23,21 +23,23 @@
 // ==/UserScript==
 
 
-/* Copyright Notice
+/* License + Copyright Notice
 ********************************************************************************************
-Copyright © TimidScript, All Rights Reserved.
-[Creative Commons BY-NC-SA](http://en.wikipedia.org/wiki/Creative_Commons_license)
+Copyright © TimidScript, Some Rights Reserved.
+GNU General Public License v3 (GPL-3) - http://www.gnu.org/licenses/gpl-3.0.en.html
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 following conditions are met:
 
-1) This copyright must be included
-2) Due credits and link to original author's homepage (included in copyright).
-3) Notify the original author of redistribution
+1) GPL-3 License is met
+2) This copyright must be included
+3) Due credits and link to original author's homepage (included in copyright).
+4) Notify the original author of redistribution
+5) Clear clarification to end user of the GPL-3 license
 
 TimidScript's Homepages:  [GitHub](https://github.com/TimidScript)
                           [OpenUserJS](https://openuserjs.org/users/TimidScript)
-                          [GreasyFork](https://greasyfork.org/users/1455-timidscript)
+                          [GreasyFork](https://greasyfork.org/users/1455-timidscript
 */
 
 /* Information
@@ -59,6 +61,8 @@ Hotkeys:
 ----------------------------------------------
     Version History
 ----------------------------------------------
+2.2.27  (2016-03-04)
+ - Changed license to GPL-3
 2.2.26 (2015-10-05)
  - Replaced base64 bmp icon with png version
 2.2.25 (2015-08-23)
