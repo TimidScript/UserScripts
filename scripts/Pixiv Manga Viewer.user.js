51378797037cfcbe99f703061e0644effec4d209
TimidScript <xxjack@gmx.com>
2016-04-03 10:05:03 +0100

    Update Pixiv Manga Viewer.user.js

--- scripts/Pixiv Manga Viewer.user.js
+++ scripts/Pixiv Manga Viewer.user.js
@@ -5,7 +5,7 @@
 // @description     A more Powerful Pixiv Manga Viewer with no lazy loading (loads all images) and better interface. Works with Pixiv++ & Generic Image Viewer ロードし、すべての画像|いいえ遅延読み込み|マンガビューア|ダイレクトリンク|ベターインタフェース
 // @author          TimidScript
 // @homepageURL     https://openuserjs.org/users/TimidScript
-// @copyright       © 2014 TimidScript, All Rights Reserved.
+// @copyright       © 2014 TimidScript, Some Rights Reserved.
 // @license         GNU General Public License v3 (GPL-3) + Read the License inside the script
 // @include         http://www.pixiv.net/member_illust.php?mode=manga&illust_id=*
 // @require         https://openuserjs.org/src/libs/TimidScript/TSL_-_Generic.js
