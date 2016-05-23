f4ff2b7d050049b7d87c0972cc708e3c191d13b7
TimidScript <xxjack@gmx.com>
2016-05-23 16:44:57 +0100

    Create update.meta.js

---
+++ data/update.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        ΞUpdateΞ
+// @namespace   TimidScript
+// @description GitHub API Tester. Works from example.com
+// @include     http://www.example.com/
+// @version     1
+// @updateURL   
+// ==/UserScript==
+
+alert ("Hello");
