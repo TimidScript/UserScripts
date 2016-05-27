3f3e72489f6bee215c2c677eb42e5f857f3b9d14
TimidScript <xxjack@gmx.com>
2016-05-27 16:45:59 +0100

    Update Generic Image Viewer.user.js

--- scripts/Generic Image Viewer.user.js
+++ scripts/Generic Image Viewer.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name            [TS] Generic Image Viewer
 // @namespace       TimidScript
-// @version         2.2.39
+// @version         2.2.40
 // @description     A more Powerful Image Viewer with info panel support for Pixiv, deviantArt, imgur, Seiga Nico and nijie.info. NEW: Image rotation and flip added.
 // @author          TimidScript
 // @homepageURL     https://github.com/TimidScript
 // @copyright       © 2016 TimidScript, Some Rights Reserved.
-// @license         GNU General Public License v3 (GPL-3) + Read the License inside the script
+// @license         Read "License + Copyright Notice" inside Script
 // @include         *
 // @require         https://greasyfork.org/scripts/19967/code/TSL - GM_update.js
 // @require         https://greasyfork.org/scripts/19968/code/TSLibrary - Generic.js
@@ -27,19 +27,18 @@
 /* License + Copyright Notice
 ********************************************************************************************
 Copyright © TimidScript, Some Rights Reserved.
-GNU General Public License v3 (GPL-3) - http://www.gnu.org/licenses/gpl-3.0.en.html
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 following conditions are met:
 
-1) GPL-3 License is met
-2) This copyright must be included
-3) Due credits and link to original author's homepage (included in copyright).
+1) GPL-3 License is met that does not conflict with the rest of the license
+2) This notice must be included
+3) Due credits and link to original author's homepage (included in this notice).
 4) Notify the original author of redistribution
-5) Clear clarification to end user of the GPL-3 license
+5) Clear clarification of the License and Notice to the end usera
+6) Do not upload on OpenUserJS.org
 
 TimidScript's Homepages:  [GitHub](https://github.com/TimidScript)
-                          [OpenUserJS](https://openuserjs.org/users/TimidScript)
                           [GreasyFork](https://greasyfork.org/users/1455-timidscript
 */
 
@@ -63,11 +62,13 @@ TODO: Replace the video controls
 ------------------------------------
  Version History
 ------------------------------------
+2.2.40 (2016-05-27)
+ - Altered license
 2.2.39 (2016-25-25)
  - Moving to GreasyFork and preparing the removal of files from OUJS
 2.2.38 (2016-04-10)
  - updateURL added
-2.2.37  (2016-03-04)
+2.2.37 (2016-04-03)
  - Changed license to GPL-3
 2.2.36 (2016-03-28)
  - Updated URL for Google Reverse Image Search
@@ -1077,7 +1078,48 @@ var ControlHQ =
                 }
                 thumbnails.timeoutID = setTimeout(function () { document.getElementById("YoutubeThumbs").style.visibility = "hidden" }, DELAY);
             }
-        }        
+        }
+        else if (hostname.match(/\.hentai-foundry\.com/i)) //Hentai-Foundry
+        {
+            console.info("GIViewer: Hentai-Foundry");
+
+            var user, id = document.location.pathname.match(/^\/\/?.\/([^\/]+)\/(\d+)/);
+
+            if (id)
+            {
+                user = id[1];
+                id = id[2];
+            }
+            else id = location.search.match(/pid=(\d+)/)[1];
+
+            console.log(id, user);
+            if (id)
+            {
+                ControlHQ.data.imgTitle = "Illustration Page";
+                ControlHQ.data.imgURL = "http://www.hentai-foundry.com/pic-" + id + ".html";
+            }
+
+            if (user)
+            {
+                ControlHQ.data.userHome = "http://www.hentai-foundry.com/user/" + user + "/profile";
+                ControlHQ.data.userName = user;
+                ControlHQ.data.userGallery = "http://www.hentai-foundry.com/pictures/user/" + user;
+            }
+
+            ControlHQ.createLinkPanel();
+        }
+        else if (hostname.match(/\w+\.facdn.net/i)) //FurAffinity
+        {
+            console.info("GIViewer: FurAffinity");
+
+            var user = pathname.match(/\/art\/(\w+)\/\d+\//i)[1];
+
+            ControlHQ.data.userHome = "http://www.furaffinity.net/user/" + user;
+            ControlHQ.data.userName = user;
+            ControlHQ.data.userGallery = "http://www.furaffinity.net/gallery/" + user;
+
+            ControlHQ.createLinkPanel();
+        }
         else ControlHQ.createLinkPanel();
 
         ControlHQ.addStyles();
