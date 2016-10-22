bdae09a1a9547bf36860795bff7e539feda98c9a
TimidScript <xxjack@gmx.com>
2016-10-22 12:09:57 +0100

    Update Youtube Translate.user.js

--- scripts/Youtube Translate.user.js
+++ scripts/Youtube Translate.user.js
@@ -5,8 +5,8 @@
 // @description     Adds translate button for video's title  and "About" description. Works in most cases
 // @author          TimidScript
 // @homepageURL     https://github.com/TimidScript
-// @copyright       © 2016 TimidScript, All Rights Reserved.
-// @license         Read "License + Copyright Notice" inside the script
+// @copyright       © 2014+ TimidScript, All Rights Reserved.
+// @license         https://github.com/TimidScript/UserScripts/blob/master/license.txt
 // @include         http*://*.youtube.*/watch?*
 // @require         https://greasyfork.org/scripts/19967/code/TSL - GM_update.js
 // @homeURL         https://greasyfork.org/en/scripts/4689
@@ -23,20 +23,23 @@
 
 /* License + Copyright Notice
 ********************************************************************************************
+License can be found at: https://github.com/TimidScript/UserScripts/blob/master/license.txt
+Below is a copy of the license the may not be up-to-date.
+
 Copyright © TimidScript, Some Rights Reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 following conditions are met:
 
-1) GPL-3 License is met that does not conflict with the rest of the license
+1) GPL-3 License is met that does not conflict with the rest of the license (http://www.gnu.org/licenses/gpl-3.0.en.html)
 2) This notice must be included
 3) Due credits and link to original author's homepage (included in this notice).
 4) Notify the original author of redistribution
-5) Clear clarification of the License and Notice to the end usera
-6) Do not upload on OpenUserJS.org
+5) Clear clarification of the License and Notice to the end user
+6) Do not upload on OpenUserJS.org or any other site that infringes on this license
 
-TimidScript's Homepages:  [GitHub](https://github.com/TimidScript)
-                          [GreasyFork](https://greasyfork.org/users/1455-timidscript
+TimidScript's Homepages:  GitHub:      https://github.com/TimidScript
+                          GreasyFork:  https://greasyfork.org/users/1455
 */
 /* Information
 ********************************************************************************************
