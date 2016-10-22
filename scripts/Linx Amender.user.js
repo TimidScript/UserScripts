796b5634b5f040babb924695cce49926e3b228e1
TimidScript <xxjack@gmx.com>
2016-10-22 11:43:48 +0100

    Update Linx Amender.user.js

--- scripts/Linx Amender.user.js
+++ scripts/Linx Amender.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name            [TS] Linx Amender
 // @namespace       TimidScript
-// @version         3.0.33
+// @version         3.0.33.1
 // @description     Generic tracking/redirection/open-in-new-tab removal; Amend page title; URL redirector; and more power functionality. Has rules for Pixiv, deviantArt, twitter, youtube, blogger, Batota etc.
 // @author          TimidScript
 // @homepageURL     https://openuserjs.org/users/TimidScript
 // @copyright       © 2016 TimidScript, Some Rights Reserved.
-// @license         Read "License + Copyright Notice" inside the script
+// @license         https://github.com/TimidScript/UserScripts/blob/master/license.txt
 // @include         *
 // @require         https://greasyfork.org/scripts/19967/code/TSL - GM_update.js
 // @require         https://greasyfork.org/scripts/19968/code/TSLibrary - Generic.js
@@ -26,20 +26,23 @@
 
 /* License + Copyright Notice
 ********************************************************************************************
+License can be found at: https://github.com/TimidScript/UserScripts/blob/master/license.txt
+Below is a copy of the license the may not be up-to-date.
+
 Copyright © TimidScript, Some Rights Reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 following conditions are met:
 
-1) GPL-3 License is met that does not conflict with the rest of the license
+1) GPL-3 License is met that does not conflict with the rest of the license (http://www.gnu.org/licenses/gpl-3.0.en.html)
 2) This notice must be included
 3) Due credits and link to original author's homepage (included in this notice).
 4) Notify the original author of redistribution
-5) Clear clarification of the License and Notice to the end usera
-6) Do not upload on OpenUserJS.org
+5) Clear clarification of the License and Notice to the end user
+6) Do not upload on OpenUserJS.org or any other site that infringes on this license
 
-TimidScript's Homepages:  [GitHub](https://github.com/TimidScript)
-                          [GreasyFork](https://greasyfork.org/users/1455-timidscript
+TimidScript's Homepages:  GitHub:      https://github.com/TimidScript
+                          GreasyFork:  https://greasyfork.org/users/1455
 */
 /* Information
 ********************************************************************************************
