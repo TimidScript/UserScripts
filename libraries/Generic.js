204c991c0472cf759c16c15f95efd8a03b86b65e
TimidScript <xxjack@gmx.com>
2017-04-18 17:04:57 +0100

    Update Generic.js

--- libraries/Generic.js
+++ libraries/Generic.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                    TSLibrary - Generic
 // @namespace               TimidScript
-// @version                 1.0.19
+// @version                 1.0.23
 // @description             A resource JS library file providing common useful functions to be used by other scripts
 // @author                  TimidScript
 // @homepageURL             https://github.com/TimidScript
@@ -35,6 +35,15 @@ TimidScript's Homepages:  GitHub:      https://github.com/TimidScript
 ********************************************************************************************
     Version History
 ----------------------------------------------
+1.0.23 (2017-03-09)
+ - Added randomNumber generator
+1.0.22 (2017-02-25)
+ - CRC32 added
+1.0.21 (2017-02-12)
+ - BugFix: TSL.padNumber
+ - Added simple checksum
+1.0.20 (2017-02-02)
+ - Added GM_xmlhttpRequest Library
 1.0.19 (2016-10-29)
  - CSS Styles are places in same position if they exists, to not break the page's style precedence. Did the same for javascript.
 1.0.18 (2016-05-27)
@@ -91,7 +100,7 @@ TimidScript's Homepages:  GitHub:      https://github.com/TimidScript
 var TimidScriptLibrary =
 {
     //http://unicode-search.net
-    ALTNTFSChars: [[">", "＞"],
+    altNTFSChars: [[">", "＞"],
                    ["<", "＜"],
                    [":", "："],
                    ['"', "＂"],
@@ -169,6 +178,13 @@ var TimidScriptLibrary =
         return str;
     },
 
+    padNumber: function (number, length)
+    {
+        //return TimidScriptLibrary.paddingLeft(number, "0", length);
+        var padding = Array(length + 1).join("0");
+        return (padding + number).slice(0 - length);
+    },
+
     isMouseEventInClientArea: function (event, element)
     {
         var rect = element.getBoundingClientRect();
@@ -216,10 +232,10 @@ var TimidScriptLibrary =
 
     replaceNTFSIllegals: function (str)
     {
-        for (var i = 0; i < TimidScriptLibrary.ALTNTFSChars.length; i++)
+        for (var i = 0; i < TimidScriptLibrary.altNTFSChars.length; i++)
         {
-            var rx = new RegExp(TimidScriptLibrary.escapeRegExp(TimidScriptLibrary.ALTNTFSChars[i][0]), "gi");
-            str = str.replace(rx, TimidScriptLibrary.ALTNTFSChars[i][1]);
+            var rx = new RegExp(TimidScriptLibrary.escapeRegExp(TimidScriptLibrary.altNTFSChars[i][0]), "gi");
+            str = str.replace(rx, TimidScriptLibrary.altNTFSChars[i][1]);
         }
 
         return str;
@@ -297,25 +313,169 @@ var TimidScriptLibrary =
     updateURL: function (url)
     {
         window.history.pushState(null, "", url);
-    }
-}
+    },
 
-//#region TimidScript Library Functions
+    makeCRCTable: function ()
+    {
+        //Code from http://stackoverflow.com/questions/18638900/javascript-crc32
+        var c;
+        var crcTable = [];
+        for (var n = 0; n < 256; n++)
+        {
+            c = n;
+            for (var k = 0; k < 8; k++)
+            {
+                c = ((c & 1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
+            }
+            crcTable[n] = c;
+        }
+        return crcTable;
+    },
+
+    crc32: function (str)
+    {
+        //Code from http://stackoverflow.com/questions/18638900/javascript-crc32
+        var crcTable = unsafeWindow.crcTable || (unsafeWindow.crcTable = TimidScriptLibrary.makeCRCTable());
+        var crc = 0 ^ (-1);
+
+        for (var i = 0; i < str.length; i++)
+        {
+            crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF];
+        }
+
+        return ((crc ^ (-1)) >>> 0).toString(16);
+    },
+
+    randomNumber: function(min, max)
+    {
+        min = parseInt(min);
+        max = parseInt(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+};
+
+
+var tsXHR =
+{
+    timeout: 15000,
+
+    /* callback(state, response)
+        state = 0 ---> success
+        state = 1 ---> Fail most likely a 404
+        state = 2 ---> Timeout
+        state = 3 ---> Error
+     -------------------------------------------------------------------*/
+    send: function (method, callback, url, headers, data, timeout)
+    {
+        //console.log(method, "\r\n", url, "\r\n", headers, "\r\n", data);
+
+        if (!headers) headers = {};
+
+        var obj = {
+            url: url,
+            method: method,
+            timeout: timeout,
+            headers: headers,
+            onload: function (response)
+            {
+                if (response.status == 200)
+                {
+                    //console.log("tsXHR: Success " + response.status + " (" + url + ")");
+                    callback(0, response);
+                }
+                else
+                {
+                    console.error("tsXHR: Fail " + response.status + " (" + url + ")");
+                    console.info(response);
+                    callback(1, response);
+                }
+            },
+            ontimeout: function (response)
+            {
+                console.error("tsXHR: Timeout" + response.status + " (" + url + ")");
+                console.info(response);
+                callback(2, response);
+            },
+            onerror: function (response)
+            {
+                console.error("tsXHR: ERROR " + response.status + " (" + url + ")");
+                console.info(response);
+                callback(3, response);
+            }
+        };
+
+        if (data) obj.data = data;
+
+        GM_xmlhttpRequest(obj);
+    },
+
+    /* callback(success, response) success true / false
+    -------------------------------------------------------------------*/
+    sendX: function (method, callback, url, headers, data, timeout)
+    {
+        tsXHR.send(method, cb, url, headers, data, timeout);
+
+        function cb(state, response)
+        {
+            callback(state == 0, response);
+        }
+    },
+
+    createHeaders: function (referer, accept, userAgent, contentType, authorization)
+    {
+        var headers = {};
+        setHeader("User-Agent", userAgent);
+        setHeader("Accept", accept);
+        setHeader("Referer", referer);
+        setHeader("Content-Type", contentType);
+        setHeader("Authorization", authorization);
+
+        return headers;
+        function setHeader(name, val)
+        {
+            if (val != null && val != undefined) headers[name] = referer;
+        }
+    },
+
+    /* callback(success, response)
+    -------------------------------------------------------------------*/
+    get: function (callback, url, headers, timeout)
+    {
+        tsXHR.sendX("GET", callback, url, headers, null, timeout);
+    },
+
+    /* callback(success, response)
+    -------------------------------------------------------------------*/
+    head: function (callback, url, headers, timeout)
+    {
+        tsXHR.sendX("GET", callback, url, headers, null, timeout);
+    },
+
+    /* callback(success, response)
+    -------------------------------------------------------------------*/
+    post: function (callback, url, headers, data, timeout)
+    {
+        tsXHR.sendX("POST", callback, url, headers, data, timeout);
+    }
+};
+
+
 /*
 Copy and paste the code underneath into your script for quick reference
-and auto-complete feature if available.
-*********************************************************************************/
+and auto-complete feature if available.*/
+//*********************************************************************************
+//#region //---------------- TimidScript Library Functions
 
 var TSL = new Object();
 
 //Remove node from document. Accepts id or node object
 TSL.removeNode = function (node, doc) { TimidScriptLibrary.removeNode(node, doc); };
-
 // Creates document element. Default doc value is the document.
 TSL.createElement = function (tag, attributes, doc) { return TimidScriptLibrary.createElement(tag, attributes, doc) };
-
 // Creates document element using html code. Default doc value is the document.
 TSL.createElementHTML = function (html, doc) { return TimidScriptLibrary.createElementHTML(html, doc) };
+//Returns the thickness of the scrollbar
+TSL.getScrollBarThickness = function () { return TimidScriptLibrary.getScrollBarThickness(); };
 
 //Add CSS styles to document header. Document can be left empty.
 TSL.addStyle = function (id, CSS, doc) { return TimidScriptLibrary.addStyle(id, CSS, doc); };
@@ -327,18 +487,11 @@ TSL.isMouseEventInClientArea = function (event, element) { return TimidScriptLib
 // Gets the position of the element within the document
 TSL.getAbsolutePosition = function (element) { return TimidScriptLibrary.getAbsolutePosition(element); };
 
-//Returns the thickness of the scrollbar
-TSL.getScrollBarThickness = function () { return TimidScriptLibrary.getScrollBarThickness(); };
-
 //Array containing NTFS illegal characters alternatives
-TSL.ALTNTFSChars = TimidScriptLibrary.ALTNTFSChars;
+TSL.altNTFSChars = TimidScriptLibrary.altNTFSChars;
 TSL.replaceNTFSIllegals = function (str) { return TimidScriptLibrary.replaceNTFSIllegals(str); };
-
+//Escape Regular expression string
 TSL.escapeRegExp = function (str) { return TimidScriptLibrary.escapeRegExp(str); };
-
-//String Padding
-String.prototype.lPad = function (chr, length) { return TimidScriptLibrary.paddingLeft(this, chr[0], length); };
-String.prototype.rPad = function (chr, length) { return TimidScriptLibrary.paddingRight(this, chr[0], length); };
 
 //Node className functions. All three functions can handle multiple classes separated by spaces
 TSL.addClass = function (node, names) { return TimidScriptLibrary.addClass(node, names); };
@@ -347,6 +500,34 @@ TSL.hasClass = function (node, names) { return TimidScriptLibrary.hasClass(node,
 
 //Allows you to change the document URL, which is reflected in the URL bar.
 TSL.updateURL = function (url) { TimidScriptLibrary.updateURL(url); };
-/*
-*********************************************************************************/
+
+//Pad a number with zeros
+TSL.padNumber = function (number, length) { return TimidScriptLibrary.padNumber(number, length); };
+//Random Whole Number Generator
+TSL.randomNumber = function (min, max) { min = parseInt(min); max = parseInt(max); return Math.floor(Math.random() * (max - min + 1) + min); };
+
+//Simple string hashing crc32
+TSL.crc32 = function (str) { return TimidScriptLibrary.crc32(str);}
+
+//String Padding
+String.prototype.lPad = function (chr, length) { return TimidScriptLibrary.paddingLeft(this, chr[0], length); };
+String.prototype.rPad = function (chr, length) { return TimidScriptLibrary.paddingRight(this, chr[0], length); };
+
+
+var XHR = new Object();
+// Default value is 10000ms
+XHR.timeout = tsXHR.timeout;
+// callback(state, response) --- state: 0 = success | 1 = Fail | 2 = Timeout | 3 = Error
+XHR.send = function (method, callback, url, headers, data, timeout) { tsXHR.send(method, callback, url, headers, data, timeout); };
+// callback(success, response) --- state: 0 = success | 1 = Fail | 2 = Timeout | 3 = Error
+XHR.sendX = function (method, callback, url, headers, data, timeout) { tsXHR.sendX(method, callback, url, headers, data, timeout); };
+// callback(success, response)
+XHR.createHeaders = function (referer, accept, userAgent, contentType, authorization) { tsXHR.createHeaders(referer, accept, userAgent, contentType, authorization) };
+// callback(success, response)
+XHR.get = function (callback, url, headers, timeout) { tsXHR.get(callback, url, headers, timeout) };
+// callback(success, response)
+XHR.head = function (callback, url, headers, timeout) { tsXHR.head(callback, url, headers, timeout) };
+// callback(success, response)
+XHR.post = function (callback, url, headers, data, timeout) { tsXHR.post(callback, url, headers, data, timeout) };
 //#endregion
+//*********************************************************************************
